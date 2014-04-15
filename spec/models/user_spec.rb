46bedfd76b7ddd1f86f352c555dc28e1b6f9c062
segeis <mail@sebastiangeis.de>
2014-04-15 18:08:27 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -99,5 +100,10 @@ describe User do
   	it { should_not be_valid }
   end
 
+  #remember token tests
+  describe "remember_token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
